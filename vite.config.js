a532d42cad9ac92411311b743cc36332eb94de08
AhmedNader7 <ahmednader.92003@gmail.com>
2026-02-03 11:08:46 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import process from 'process'
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/task-management",
+  base: process.env.VITE_BASE_PATH || "/task-management",
 });
